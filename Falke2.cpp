de65ee4c72b6dc30f3fef6ad378f60c234fe2470
AleksandraFalke <79189146+AleksandraFalke@users.noreply.github.com>
2024-04-01 01:05:18 +0700

    Add files via upload

--- Falke2.cpp
+++ Falke2.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+
 inline _int64 HightTime() {
     _asm {
         rdtsc
@@ -36,13 +37,10 @@ typedef struct _PROCESSOR_POWER_INFORMATION {
     ULONG  CurrentIdleState;
 } PROCESSOR_POWER_INFORMATION, * PPROCESSOR_POWER_INFORMATION;
 
-//int n = 500;
-
 int main()
 {
-    //unsigned int start_time = clock(); // начальное время
     SPEED_START_CONSOLE
-    int n = 500;
+    int n = 50000000;
     int K = 500;
     double* A = new double[n];
     double times;
@@ -52,11 +50,11 @@ int main()
     }
     for (int i = 0; i < n; i++){
         A[rand() % n] = A[rand() % n] + K;
+        //cout << A[i] << endl;
     }
-    SPEED_FINISH_CONSOLE // скорость производительности в тактах
-    //unsigned int end_time = clock(); // конечное время
-    //unsigned int search_time = end_time - start_time; // искомое время
-    //cout << search_time << " second" << endl;
+    std::cout << "Without output of array elements(50,000,000)" << endl;;
+    std::cout << "clock  ";
+    SPEED_FINISH_CONSOLE // performance speed in clock 
 
     SYSTEM_INFO si = { 0 };
     GetSystemInfo(&si);
@@ -67,7 +65,7 @@ int main()
 
     CallNtPowerInformation(ProcessorInformation, NULL, 0, &a[0], dwSize);
 
-    std::cout << a[0].CurrentMhz << " Mhz" << endl; // Тактовая частота
+    std::cout << a[0].CurrentMhz << " Mhz" << endl; // Clock frequency
 
     times = (pft_finish - pft_start) / (a[0].CurrentMhz * 1000000.0);
 
@@ -79,4 +77,3 @@ int main()
     a.clear();
     return 0;
 }
-//cout << A[i] << endl;
